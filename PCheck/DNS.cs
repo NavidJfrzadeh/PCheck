6fc2427838b83a17aa2a1bcd0efc6195f77202d3
NavidJafarzadeh <navid.jfrzadeh@gmail.com>
2026-05-01 23:27:04 +0330

    Add project files.

---
+++ PCheck/DNS.cs
@@ -0,0 +1,8 @@
+﻿namespace PCheck;
+
+public class DNS(string address, string title)
+{
+    public int Id { get; set; }
+    public string Address { get; set; } = address;
+    public string Title { get; set; } = title;
+}
---
+++ PCheck/Tools.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+namespace PCheck;
+
+public static class Tools
+{
+    private static readonly string _path = "dnsList.txt";
+    public static void SaveToFile(DNS newDns)
+    {
+        var data = JsonSerializer.Serialize(newDns);
+        File.AppendAllText(_path, data);
+    }
+
+    public static List<DNS> GetAllDnsFromFile()
+    {
+        var data = File.ReadAllText(_path);
+        return JsonSerializer.Deserialize<List<DNS>>(data) ?? new List<DNS>();
+    }
+}
